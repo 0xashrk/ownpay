25871279db5c19ed414d7f2193c452092ceb0a97
Ash <ashwinravikumarv@gmail.com>
2025-03-02 18:20:02 -0700

    Initial Commit

---
+++ Tap/TapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapApp.swift
+//  Tap
+//
+//  Created by Ashwin Ravikumar on 02/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
