a4a22c6b8dc8d5d91046eac8c4b792394692d9d4
Ash <ashwinravikumarv@gmail.com>
2025-03-11 21:03:56 -0400

    Create MerchantView with Request and Send buttons
    - Add new MerchantView Swift file
    - Implement Request Payment and Send MON buttons
    - Use consistent styling with blue color and rounded corners
    - Include UISelectionFeedbackGenerator for button interactions

---
+++ Tap/Views/MerchantView.swift
@@ -0,0 +1,18 @@
+//
+//  MerchantView.swift
+//  Own Pay
+//
+//  Created by Ashwin Ravikumar on 11/03/2025.
+//
+
+import SwiftUI
+
+struct MerchantView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MerchantView()
+}
